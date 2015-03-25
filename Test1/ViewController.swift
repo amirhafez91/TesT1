fce118177ed4f7fbf9e5cc6c9464c42b3b17357f
Amir Hafez <ahafez@Amirs-MacBook.lan>
2015-03-25 18:46:20 +0200

    added a comment

--- Test1/ViewController.swift
+++ Test1/ViewController.swift
@@ -33,7 +33,7 @@ class ViewController: UIViewController {
         MailButton.backgroundColor=UIColor.redColor()
         EnterMessageTextField.text=""
         EnterMessageTextField.resignFirstResponder()
-        
+        //hjfxfcghjklhgfxfghj
         
         
     }
