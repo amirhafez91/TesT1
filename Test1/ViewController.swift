92808a5c0f7daa580bdfb48986b581dbc65e4c4f
Amir Hafez <ahafez@Amirs-MacBook.lan>
2015-03-25 18:39:14 +0200

    first rev of project

---
+++ Test1/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Test1
+//
+//  Created by Amir Hafez on 3/25/15.
+//  Copyright (c) 2015 Amir Hafez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var EnterNameTextField: UITextField!
+    @IBOutlet weak var EnterMessageTextField: UITextField!
+    @IBOutlet weak var MailButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        messageLabel.hidden=false
+        messageLabel.text=EnterNameTextField.text+" "+EnterMessageTextField.text
+        messageLabel.textColor=UIColor.redColor()
+        MailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        MailButton.backgroundColor=UIColor.redColor()
+        EnterMessageTextField.text=""
+        EnterMessageTextField.resignFirstResponder()
+        
+        
+        
+    }
+
+}
+
